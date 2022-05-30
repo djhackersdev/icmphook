e9aa90f2d2788e11390cd3ebed7c84f03667b36e
Tau <tau@nowhere>
2022-05-30 15:31:22 +0100

    chore: set up project

---
+++ icmphook/dllmain.c
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+BOOL WINAPI DllMain(HMODULE mod, DWORD cause, void *ctx)
+{
+    return TRUE;
+}
---
+++ precompiled.h
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
